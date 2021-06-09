4beda24ccdafe9a525457df35f14c28e40e637b7
Naveen S <naveensnlbe@gmail.com>
2021-06-10 00:04:59 +0530

    Added the Notification and Readme title

---
+++ README.md
@@ -0,0 +1,3 @@
+## Cowin Vaccine API Python
+
+This is the Project to get the vaccine availability from the Cowin Site.
--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import requests
-import json
+from pynotifier import Notification
+
 
 cowin_requests = requests.get(
     "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=265&date=06-06-2021")
 
 cowin_json = cowin_requests.json()
 
-# print(cowin_json['centers'][0]['sessions'][0])
-
 no_vaccines_available = True
 
-for cowin_centers in cowin_json['centers']:
-    if cowin_centers['fee_type'] == 'Free':
-        for cowin_sessions in cowin_centers['sessions']:
-            if(cowin_sessions['min_age_limit'] == 18):
-                if(cowin_sessions['available_capacity'] > 0):
-                    no_vaccines_available = False
-                    print(cowin_sessions['available_capacity'])
 
-if no_vaccines_available:
-    print('No Vaccines Available')
+def notification(title, message):
+    Notification(
+        title=title,
+        description=message,
+        # On Windows .ico is required, on Linux - .png
+        icon_path='path/to/image/file/icon.png',
+        duration=5,                              # Duration in seconds
+        urgency='normal'
+    ).send()
+
+
+def checkVaccineAvailability():
+    for cowin_centers in cowin_json['centers']:
+        if cowin_centers['fee_type'] == 'Free':
+            for cowin_sessions in cowin_centers['sessions']:
+                if(cowin_sessions['min_age_limit'] == 18):
+                    if(cowin_sessions['available_capacity'] > 0):
+                        no_vaccines_available = False
+                        print(cowin_sessions['available_capacity'])
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        checkVaccineAvailability()
+
+        # notification(title='Notification Title', message='Notification Description')
+
+        if no_vaccines_available:
+            print('No Vaccines Available')
