40ba7d756075cc42199a191a78ef9d12038369cf
Naveen S <naveensnlbe@gmail.com>
2021-06-15 22:20:23 +0530

    Added the Next 7 days date

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import requests
 from pynotifier import Notification
+from datetime import datetime,timedelta
+# Manually need to change district id
+district_id = '293'
+# Need to create 8 days array from current data format: DD-MM-YYYY
+# current_date = datetime.today().strftime('%d-%m-%Y')
+# end_date = current_date + datetime.timedelta(days=10)
+# print(end_date)
 
 
-cowin_requests = requests.get(
-    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=265&date=06-06-2021")
+# create requests as a function
 
-cowin_json = cowin_requests.json()
+def getData(getDate):
+    cowin_requests = requests.get(
+    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+district_id+"&date="+getDate+"")
+    cowin_json = cowin_requests.json()
+    return cowin_json
+
 
 no_vaccines_available = True
 
@@ -15,29 +26,42 @@ def notification(title, message):
         title=title,
         description=message,
         # On Windows .ico is required, on Linux - .png
-        icon_path='path/to/image/file/icon.png',
+        icon_path='images/logo_1.ico',
         duration=5,                              # Duration in seconds
         urgency='normal'
     ).send()
 
 
-def checkVaccineAvailability():
-    for cowin_centers in cowin_json['centers']:
+def checkVaccineAvailability(covid_json):
+    for cowin_centers in covid_json['centers']:
         if cowin_centers['fee_type'] == 'Free':
             for cowin_sessions in cowin_centers['sessions']:
                 if(cowin_sessions['min_age_limit'] == 18):
                     if(cowin_sessions['available_capacity'] > 0):
+                        # Don't need to use global function or variables
                         no_vaccines_available = False
-                        print(cowin_sessions['available_capacity'])
+                        # Hospital Detail
+                        # Call the Notification in messge
+                        print()
+                        notification(title='covid 19 vaccine', message=cowin_centers['name']+' '+'Hospital has covid 19 vaccines');
+
+                        # print(cowin_sessions['available_capacity'])
 
 
 if __name__ == "__main__":
 
-    while True:
+    # while True:
 
-        checkVaccineAvailability()
+    today = datetime.now()
+    for i in range(0,8):
+        day = today + timedelta(days=i)
+        # print(day.strftime("%d-%m-%Y"))
+        json = getData(day.strftime("%d-%m-%Y"))
 
-        # notification(title='Notification Title', message='Notification Description')
+        checkVaccineAvailability(json)
 
-        if no_vaccines_available:
-            print('No Vaccines Available')
+
+    if no_vaccines_available:
+        print('No Vaccines Available')
+
+    # After 1 Min need check again
